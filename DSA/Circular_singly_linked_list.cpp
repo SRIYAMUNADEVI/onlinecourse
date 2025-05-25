269feba147479c99723b58e1bc23a76f70ca770c
SRIYAMUNADEVI <144534419+SRIYAMUNADEVI@users.noreply.github.com>
2025-05-25 07:31:10 +0530

    Add files via upload

---
+++ DSA/Circular_singly_linked_list.cpp
@@ -0,0 +1,138 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+/************************datatype for node*************************************/
+struct Node {
+	int data;
+	Node *next;
+};
+/***************************Head Node creation*********************************/
+Node *head = NULL;
+/**************************Create new node*************************************/
+Node* createnew_Node(int value)
+{
+	struct Node* temp = new Node();
+	temp->data = value;
+	temp->next = NULL;
+	return temp;
+}
+/*****************************display list*************************************/
+void display()
+{
+	struct Node* temp = head;
+	cout << "Head->";
+	if(temp == NULL)
+	{
+		cout << "NULL";
+	}
+	else
+	{
+		cout << temp->data << "->";
+		while(temp->next != head)
+		{
+			temp = temp->next;
+			cout << temp->data << "->";
+		}
+		cout<< "Head";
+	}
+	cout << endl;
+}
+/*************************Insert nodes at the beginning************************/
+void insert(int value)
+{
+	struct Node* temp = createnew_Node(value);
+	struct Node* iterator = NULL;
+	if(head == NULL)
+	{
+		head = temp;
+		head->next = head;
+	}
+	else
+	{
+		iterator = head;
+		while(iterator->next != head)
+		{
+			iterator = iterator->next;
+		}
+		iterator->next = temp;
+		temp->next = head;
+		head = temp;
+	}
+}
+
+void insertAtSpecificPosition(int value, int pos)
+{
+	struct Node* temp = head;
+	struct Node* local = NULL;
+	if(head == NULL)
+	{
+		insert(value);
+	}
+	else
+	{
+		while(((pos-1)!=0) && (temp->next!=NULL))
+		{
+			temp = temp->next;
+			pos--;
+		}
+		local = createnew_Node(value);
+		local->next = temp->next;
+		temp->next = local;
+	}
+}
+/****************************Insert a node at the end**************************/
+void insertAtEnd(int value)
+{
+	struct Node* iterator = head;
+	struct Node* temp =NULL;
+	temp = createnew_Node(value);
+	while(iterator->next != head)
+	{
+		iterator = iterator->next;
+	}
+	iterator->next = temp;
+	temp->next = head;
+}
+
+int main()
+{
+	// Insert nodes at the beginning
+	insert(3);
+	cout << "After inserting 3 at the beginning: "<< endl;
+	display();
+
+	insert(1);
+	cout << "After inserting 1 at the beginning: "<< endl;
+	display();
+
+	insert(7);
+	cout << "After inserting 7 at the beginning: "<< endl;
+	display();
+
+	insert(2);
+	cout << "After inserting 2 at the beginning: "<< endl;
+	display();
+
+	insert(9);
+	cout << "After inserting 9 at the beginning: "<< endl;
+	display();
+
+	// Insert a node at a specific position
+	insertAtSpecificPosition(5, 2); // Insert 5 at position 2
+	cout << "After inserting 5 at position 2: "<< endl;
+	display();
+
+	// Insert a node at the end
+	insertAtEnd(10);
+	cout << "After inserting 10 at the end: "<< endl;
+	display();
+
+	return 0;
+}
